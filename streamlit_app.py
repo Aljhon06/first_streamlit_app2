478d94d4f7a71bbdc29a9e4ed6cdee7d70896139
Aljhon06 <145524253+Aljhon06@users.noreply.github.com>
2023-09-21 14:36:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,11 +35,11 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-streamlit.write('The user entered ', fruit_choice)
 
 except URLError as e:
   streamlit.error()
-
+  
+streamlit.write('The user entered ', fruit_choice)
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json()) # just write the data on the screen
